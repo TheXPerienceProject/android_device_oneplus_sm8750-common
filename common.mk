2309f201bb887864cd806d933049fb891bd06d1e
LuK1337 <priv.luk@gmail.com>
2025-09-29 17:23:57 +0200

    sm8750-common: Exclude NXP keymint/weaver from tablet builds
    erhai does not have hardware required to use these.
    
    Change-Id: Ic647a414981e67a44204d3570e49667c402fd341

--- common.mk
+++ common.mk
@@ -236,9 +236,13 @@ PRODUCT_COPY_FILES += \
 
 # Keymint
 PRODUCT_PACKAGES += \
-    android.hardware.hardware_keystore_V3.xml \
+    android.hardware.hardware_keystore_V3.xml
+
+ifneq ($(TARGET_IS_TABLET),true)
+PRODUCT_PACKAGES += \
     android.hardware.security.keymint3-service.strongbox.nxp \
     android.hardware.weaver-service.nxp
+endif
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml \
