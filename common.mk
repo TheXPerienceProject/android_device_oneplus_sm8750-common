db82bdbef34235fd845e479bca930ede7f4b170e
klozz <carlosj@klozz.dev>
2026-04-02 01:37:04 -0600

    SM8750-common: health:Add bypass support
    Change-Id: I8b00fc88c3a5d523efcf3cbb6bf221fe80882876
    Signed-off-by: klozz <carlosj@klozz.dev>

--- common.mk
+++ common.mk
@@ -278,6 +278,7 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/oplus_chg/battery/mmi_charging_enable)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,true)
 
 # Ltpo
 PRODUCT_PACKAGES += \
